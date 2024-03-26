936822c4ea3282a55e65224037558c368dbd9567
l1728 <cheburashka077@ukr.net>
2024-03-27 01:13:53 +0200

    add library

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,1 +1,9 @@
-console.log("Timer");
+// Описаний в документації
+import flatpickr from "flatpickr";
+// Додатковий імпорт стилів
+import "flatpickr/dist/flatpickr.min.css";
+
+// Описаний у документації
+import iziToast from "izitoast";
+// Додатковий імпорт стилів
+import "izitoast/dist/css/iziToast.min.css";
--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,4 @@
-console.log("Snackbar");
+// Описаний у документації
+import iziToast from "izitoast";
+// Додатковий імпорт стилів
+import "izitoast/dist/css/iziToast.min.css";
